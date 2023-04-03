f749a8f278793d348bd91c2a4a11d39ff060d712
alvaroherbon <alvaroherbon93@gmail.com>
2023-04-03 15:46:21 +0200

    minor changes

--- src/app/aedo/components/list-languages/list-languages.component.ts
+++ src/app/aedo/components/list-languages/list-languages.component.ts
@@ -4,7 +4,8 @@ import { ILanguage } from '../../interfaces/language.interface';
 import { Language } from '../../models/language.model';
 import { LanguagesService } from '../../services/models-services/languages.service';
 import { MatDialog } from '@angular/material/dialog';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-list-languages',
@@ -18,16 +19,31 @@ export class ListLanguagesComponent implements OnInit {
   private selectedLanguage: ILanguage = new Language('', '');
   private newLanguage: ILanguage = new Language('', '');
 
+  @ViewChild('closeNewLanguageModal') closeModal: any;
   newLanguageForm: FormGroup;
   editLanguageForm: FormGroup;
 
   constructor(private languagesService: LanguagesService) {
     this.newLanguageForm = new FormGroup({
-      newLanguageItem: new FormControl(''),
+      newLanguageItem: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
     });
     this.editLanguageForm = new FormGroup({
-      editLanguageItem: new FormControl(''),
+      editLanguageItem: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
     });
+  }
+
+  get editLanguageItem() {
+    return this.editLanguageForm.get('editLanguageItem');
+  }
+
+  get newLanguageItem() {
+    return this.newLanguageForm.get('newLanguageItem');
   }
 
   ngOnInit(): void {}
@@ -40,13 +56,23 @@ export class ListLanguagesComponent implements OnInit {
     return this.newLanguage;
   }
 
-  public createLanguage(): void {
+  public async createLanguage(): Promise<void> {
     this.newLanguage.item = this.newLanguageForm.value.newLanguageItem;
     const id = this.newLanguage.item.toUpperCase().substring(0, 3);
     this.newLanguage.id = id;
-    this.languagesService.create(this.newLanguage);
-
-    //TODO - comprobar si existe el lenguage, y en ese caso devolver un error
+    await this.languagesService.getById(id).then((res) => {
+      console.log('el resultado es: ' + res.id);
+      try {
+        if (res.item) {
+          throw new Error('Language already exists');
+        } else {
+          this.languagesService.create(this.newLanguage);
+          this.closeModal.click();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
   }
 
   public getSelectedLanguage(): ILanguage {
--- src/app/aedo/services/models-services/languages.service.ts
+++ src/app/aedo/services/models-services/languages.service.ts
@@ -4,31 +4,36 @@ import { Observable } from 'rxjs';
 import { ILanguage } from '../../interfaces/language.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LanguagesService {
+  collection: string = 'languages';
 
-  collection:string = "languages"
+  constructor(private firestoreService: FirestoreService) {}
 
-  constructor(private firestoreService:FirestoreService) { }
-
-  create(language: ILanguage){
-    return this.firestoreService.set(this.collection,language)
+  create(language: ILanguage) {
+    return this.firestoreService.set(this.collection, language);
   }
 
-  getCollection():Observable<ILanguage[]>{
-    return this.firestoreService.getCollection(this.collection) as Observable<ILanguage[]>
+  getCollection(): Observable<ILanguage[]> {
+    return this.firestoreService.getCollection(this.collection) as Observable<
+      ILanguage[]
+    >;
   }
 
-  remove(language: ILanguage){
-    return this.firestoreService.remove(this.collection,language.id)
+  remove(language: ILanguage) {
+    return this.firestoreService.remove(this.collection, language.id);
   }
 
-  async update(language: ILanguage){
-    this.firestoreService.update(this.collection,language, language.id)
+  async update(language: ILanguage) {
+    this.firestoreService.update(this.collection, language, language.id);
   }
 
-  async getById(id:string): Promise<ILanguage>{
-    return await this.firestoreService.getById(this.collection,id) as ILanguage
+  //TODO - NO FUNCIONA BIEN ESTE MÉTODO
+  async getById(id: string): Promise<ILanguage> {
+    return (await this.firestoreService.getById(
+      this.collection,
+      id
+    )) as ILanguage;
   }
 }
