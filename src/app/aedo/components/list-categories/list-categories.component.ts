c1f4455fb1e00f8194b0a1737448db3c456d4744
alvaroherbon <57678071+alvaroherbon@users.noreply.github.com>
2023-04-05 20:26:02 +0200

    minor changes

--- src/app/aedo/components/list-categories/list-categories.component.ts
+++ src/app/aedo/components/list-categories/list-categories.component.ts
@@ -4,6 +4,7 @@ import { CategoryService } from '../../services/models-services/category.service
 import { Observable } from 'rxjs';
 import { ICategory } from '../../interfaces/category.interface';
 import { ListLanguagesComponent } from '../list-languages/list-languages.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list-categories',
@@ -11,13 +12,50 @@ import { ListLanguagesComponent } from '../list-languages/list-languages.compone
   styleUrls: ['./list-categories.component.css'],
 })
 export class ListCategoriesComponent implements OnInit {
-  constructor(private categoryService: CategoryService) {}
+
+  closeResult: string = '';
+  SelectedCategory: ICategory = new Category('');
+
+  constructor(
+    private categoryService: CategoryService,
+    private modalService: NgbModal
+  ) {}
 
   private listCategories: Observable<ICategory[]> =
     this.categoryService.getCollection();
+
+  public getListCategories(): Observable<ICategory[]> {
+    return this.listCategories;
+  }
+
   ngOnInit(): void {
     this.listCategories.subscribe((data) => {
       console.log('data', data);
     });
   }
+
+  open(content: any, category: ICategory = new Category('')) {
+    if (category.name) {
+      this.setSelectedCategory(category);
+    }
+    this.modalService.open(content, { ariaLabelledBy: 'modal' }).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+        if (result === 'removeCategory') {
+          this.removeCategory(category.id);
+        }
+      }
+    );
+  }
+  setSelectedCategory(category: ICategory) {
+    this.SelectedCategory = category;
+  }
+
+  public async removeCategory(id: string): Promise<void> {
+    await this.categoryService.remove(id);
+  }
+
+  getSelectedCategory() {
+    return this.SelectedCategory; 
+    }
 }
