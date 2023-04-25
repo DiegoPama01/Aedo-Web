251f079919797ef2ef9d325feb2a245d72f34ab6
alvaroherbon <alvaroherbon93@gmail.com>
2023-04-25 08:34:20 +0200

    added functions and emulators

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,6 @@ import * as functions from "firebase-functions";
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.objectFinalized = functions.storage.object().onFinalize((object) => {
+    return console.log('File name is: ', object.name);
+});
