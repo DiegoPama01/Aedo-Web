183934a6f8a211dc9d1f71657cf4e9d013ab9b4d
DiegoPama <105394232+DiegoPama01@users.noreply.github.com>
2023-05-19 23:34:16 +0200

    Añadido custom theme y fire auth guard

--- src/app/aedo/aedo-routing.module.ts
+++ src/app/aedo/aedo-routing.module.ts
@@ -1,35 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { canActivate, redirectUnauthorizedTo} from '@angular/fire/auth-guard'
 import { AdministratorComponent } from './pages/administrator/administrator.component';
 
 import { HomeComponent } from './pages/home/home.component';
 import { OdiseaRegisterComponent } from './pages/odisea-register/odisea-register.component';
 import { ProfileViewComponent } from './pages/profile-view/profile-view.component';
 
-
-
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
   {
-    path: '',
+    path: 'home',
     component: HomeComponent,
     children: [
       {
-        path:"administrator",
-        component:AdministratorComponent
+        path: 'administrator',
+        component: AdministratorComponent,
+        ...canActivate(() => redirectUnauthorizedTo(['/home']))
       },
       {
-        path:"registration",
-        component:OdiseaRegisterComponent
+        path: 'registration',
+        component: OdiseaRegisterComponent,
+        ...canActivate(() => redirectUnauthorizedTo(['/home']))
       },
       {
-        path:"profile",
-        component:ProfileViewComponent
-      },
-      {
-        path:"registerOdisea",
-        component:OdiseaRegisterComponent
+        path: 'profile',
+        component: ProfileViewComponent,
+        ...canActivate(() => redirectUnauthorizedTo(['/home']))
       }
-
     ],
   },
 ];
